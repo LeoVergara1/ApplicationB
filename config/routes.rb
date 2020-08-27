9e5d46485079c451ee4f97a37b677dbae872920c
Brandon Vergara <leovergara.dark@gmail.com>
2020-08-27 15:19:01 -0500

    coding master  creating route test

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :shippings
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope 'delivery_tracking' do
+    get '/:delivery' => 'delivery_tracking#shipp'
+  end
 end
