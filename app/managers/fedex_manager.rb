d9434ce35de6f6723f4b91c0bea1ff64c4b4496a
Brandon Vergara <leovergara.dark@gmail.com>
2020-08-27 20:48:00 -0500

    coding   init fedex gem

--- app/managers/fedex_manager.rb
+++ app/managers/fedex_manager.rb
@@ -1,7 +1,16 @@
 require 'singleton'
+require 'fedex'
 
 class FedexManager < DeliveryFactory
   include Singleton
+
+  def initialize
+    @fedex = Fedex::Shipment.new(:key => 'O21wEWKhdDn2SYyb',
+                                 :password => 'db0SYxXWWh0bgRSN7Ikg9Vunz',
+                                 :account_number => '510087780',
+                                 :meter => '119009727',
+                                 :mode => 'test')
+  end
 
   def tracking
     p "Helo word"
