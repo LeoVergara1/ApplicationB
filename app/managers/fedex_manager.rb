d2a69fa21d7ac587378288808e10adf8846c6b65
Brandon Vergara <leovergara.dark@gmail.com>
2020-08-28 14:47:19 -0500

    coding master  add module to test bunny

--- app/managers/fedex_manager.rb
+++ app/managers/fedex_manager.rb
@@ -1,5 +1,6 @@
 require 'singleton'
 require 'fedex'
+require "bunny"
 
 class FedexManager < DeliveryFactory
   include Singleton
@@ -10,10 +11,20 @@ class FedexManager < DeliveryFactory
                                  :account_number => '510087780',
                                  :meter => '119009727',
                                  :mode => 'test')
+
+    @conn = Bunny.new("amqps://csaizoxi:qv_k0WA2C6LxW62pKsOnacjGQhgnBaXN@coyote.rmq.cloudamqp.com/csaizoxi")
+    @conn.start
+    @ch = @conn.create_channel
+    @q  = @ch.queue("brandon")
   end
 
   def tracking(tracking_number)
-    @fedex.track(:tracking_number => tracking_number)
+    begin
+      @fedex.track(:tracking_number => tracking_number)
+    rescue => exception
+      p exception
+      p "Ocurrer error"
+    end
   end
 
   def rate(shipping_fedex)
@@ -30,5 +41,9 @@ class FedexManager < DeliveryFactory
       :packages => shipping_fedex.packages,
       :service_type => "FEDEX_GROUND",
       :shipping_options => shipping_fedex.shipping_options)
+  end
+
+  def find
+    @q.publish("Hello, everybody #{Time.new}!")
   end
 end
