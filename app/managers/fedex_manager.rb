dee892e8f0a611556dbe155de0dba2dd33e72dfb
Brandon Vergara <leovergara.dark@gmail.com>
2020-08-28 00:52:14 -0500

    coding   add ship and rate method to get info from fedex

--- app/managers/fedex_manager.rb
+++ app/managers/fedex_manager.rb
@@ -12,7 +12,23 @@ class FedexManager < DeliveryFactory
                                  :mode => 'test')
   end
 
-  def tracking
-    p "Helo word"
+  def tracking(tracking_number)
+    @fedex.track(:tracking_number => tracking_number)
+  end
+
+  def rate(shipping_fedex)
+    @fedex.rate(:shipper=>shipping_fedex.shipper,
+      :recipient => shipping_fedex.recipient,
+      :packages => shipping_fedex.packages,
+      :service_type => "FEDEX_GROUND",
+      :shipping_options => shipping_fedex.shipping_options)
+  end
+
+  def ship(shipping_fedex)
+    @fedex.ship(:shipper=> shipping_fedex.shipper,
+      :recipient => shipping_fedex.recipient,
+      :packages => shipping_fedex.packages,
+      :service_type => "FEDEX_GROUND",
+      :shipping_options => shipping_fedex.shipping_options)
   end
 end
