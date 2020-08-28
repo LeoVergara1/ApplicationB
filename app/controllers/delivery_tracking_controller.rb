2ce5b1cef4fdaf87e27a18490225bacb3f59fc93
Brandon Vergara <leovergara.dark@gmail.com>
2020-08-28 12:06:27 -0500

    coding   check status enum

--- app/controllers/delivery_tracking_controller.rb
+++ app/controllers/delivery_tracking_controller.rb
@@ -3,4 +3,8 @@ class DeliveryTrackingController < ApplicationController
   def shipp
     render json: {"nombre" => "Germán"}
   end
+
+  def satatus_delivery
+    render json: {"status" => DeliveryStatus::EXCEPTION}
+  end
 end
