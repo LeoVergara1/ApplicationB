11d34bca94433802efa5c30441a9bebae501216e
Brandon Vergara <leovergara.dark@gmail.com>
2020-08-27 20:13:33 -0500

    coding   adding methods to factory

--- app/managers/delivery_factory.rb
+++ app/managers/delivery_factory.rb
@@ -1,2 +1,10 @@
 class DeliveryFactory 
+
+  def tracking
+    raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
+  end
+
+  def delivered
+    raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
+  end
 end
