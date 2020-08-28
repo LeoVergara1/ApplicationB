f22f1823d7459fb5fedfe9cb064862aae540e234
Brandon Vergara <leovergara.dark@gmail.com>
2020-08-28 14:46:40 -0500

    coding master  add new route to consume status

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   scope 'delivery_tracking' do
     get '/:delivery' => 'delivery_tracking#shipp'
+    get '/satatus_delivery/:delivery' => 'delivery_tracking#satatus_delivery'
   end
 end
